8d5ae046040fed3a715072a75ab5d1903417d98e
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2021-10-03 18:05:12 -0500

    not sure what changed here

--- hippyflow/utilities/__init__.py
+++ hippyflow/utilities/__init__.py
@@ -13,7 +13,7 @@
 
 from .mesh_utils import read_serial_write_parallel_mesh
 
-from .mv_utilities import mv_to_dense
+from .mv_utilities import mv_to_dense, dense_to_mv_local
 
 from .plotting import *
 
