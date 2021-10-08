f650d2c2073ba9189d21c570465163a091f6cdf7
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2021-10-08 15:56:49 -0500

    exploring all these different ways travis doesnt make any sense

--- hippyflow/test/test_derivativeSubspace.py
+++ hippyflow/test/test_derivativeSubspace.py
@@ -18,12 +18,12 @@ import numpy as np
 
 
 import sys, os
+sys.path.append(os.environ.get('HIPPYFLOW_PATH'))
 # sys.path.append(os.environ.get('HIPPYLIB_PATH'))
-from hippylib import *
+import hippylib as hl
 
 # sys.path.append(os.environ.get('HIPPYFLOW_PATH'))
-# sys.path.append('../../')
-from hippyflow import *
+import hippyflow as hf
 
 def u_boundary(x, on_boundary):
 	return on_boundary and ( x[1] < dl.DOLFIN_EPS or x[1] > 1.0 - dl.DOLFIN_EPS)
@@ -38,7 +38,7 @@ class TestDerivativeSubspace(unittest.TestCase):
 
 		self.observable = self.buildObservable(self.mesh)
 
-		self.prior = BiLaplacian2D(self.Vh[PARAMETER],gamma = 0.1, delta = 1.0)
+		self.prior = hf.BiLaplacian2D(self.Vh[hl.PARAMETER],gamma = 0.1, delta = 1.0)
 
 
 	
@@ -53,13 +53,13 @@ class TestDerivativeSubspace(unittest.TestCase):
 			
 		u_bdr = dl.Expression("x[1]", degree=1)
 		u_bdr0 = dl.Constant(0.0)
-		bc = dl.DirichletBC(self.Vh[STATE], u_bdr, u_boundary)
-		bc0 = dl.DirichletBC(self.Vh[STATE], u_bdr0, u_boundary)
+		bc = dl.DirichletBC(self.Vh[hl.STATE], u_bdr, u_boundary)
+		bc0 = dl.DirichletBC(self.Vh[hl.STATE], u_bdr0, u_boundary)
 		
 		def pde_varf(u,m,p):
 			return ufl.exp(m)*ufl.inner(ufl.grad(u), ufl.grad(p))*ufl.dx - f*p*ufl.dx
 
-		self.pde = PDEVariationalProblem(self.Vh, pde_varf, bc, bc0, is_fwd_linear=True)
+		self.pde = hl.PDEVariationalProblem(self.Vh, pde_varf, bc, bc0, is_fwd_linear=True)
 
 
 		x_targets = np.linspace(0.1,0.9,10)
@@ -70,8 +70,8 @@ class TestDerivativeSubspace(unittest.TestCase):
 				targets.append((xi,yi))
 		targets = np.array(targets)
 
-		B = assemblePointwiseObservation(self.Vh[STATE], targets)
-		return LinearStateObservable(self.pde,B)
+		B = hl.assemblePointwiseObservation(self.Vh[hl.STATE], targets)
+		return hf.LinearStateObservable(self.pde,B)
 
 
 		
@@ -81,16 +81,16 @@ class TestDerivativeSubspace(unittest.TestCase):
 		"""
 		Test the agreement of the two different routines for computing active subspace
 		"""
-		AS_parameters = ActiveSubspaceParameterList()
+		AS_parameters = hf.ActiveSubspaceParameterList()
 		AS_parameters['observable_constructor'] = self.buildObservable
 		AS_parameters['observable_kwargs'] = {}
 		AS_parameters['save_and_plot'] = False
 		AS_parameters['serialized_sampling'] = False
 		AS_parameters['store_Omega'] = True
 		AS_parameters['rank'] = 64
-		my_collective = NullCollective()
+		my_collective = hf.NullCollective()
 
-		AS = ActiveSubspaceProjector(self.observable,self.prior,collective = my_collective,parameters = AS_parameters)
+		AS = hf.ActiveSubspaceProjector(self.observable,self.prior,collective = my_collective,parameters = AS_parameters)
 		# Construct the subspace via batching
 		AS.construct_input_subspace()
 		d_batch_in = AS.d_GN
