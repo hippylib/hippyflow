bce9f5cee861153f2055bdb0b700ddc9540dca1c
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2021-10-08 13:19:32 -0500

    updating

--- hippyflow/test/test_derivativeSubspace.py
+++ hippyflow/test/test_derivativeSubspace.py
@@ -22,6 +22,7 @@ sys.path.append(os.environ.get('HIPPYLIB_PATH'))
 from hippylib import *
 
 sys.path.append(os.environ.get('HIPPYFLOW_PATH'))
+sys.path.append('../../')
 from hippyflow import *
 
 def u_boundary(x, on_boundary):
