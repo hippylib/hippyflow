2a5301e5bfb99e665e38cb99033729478d435035
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2024-06-06 18:11:02 -0500

    need to save pod

--- hippyflow/modeling/dataGenerator.py
+++ hippyflow/modeling/dataGenerator.py
@@ -257,6 +257,13 @@ class DataGenerator:
 			orth_error = np.linalg.norm(PsistarPsi - np.eye(PsistarPsi.shape[0]))
 			print('||Psi^*Psi - I|| = ',orth_error)
 			assert orth_error < 1e-5
+		# Save POD
+		os.makedirs(data_dir+'/POD/',exist_ok=True)
+		Phi_np = hf.mv_to_dense(Phi)
+		MPhi_np = hf.mv_to_dense(MPhi)
+		np.save(data_dir+f'J_data/POD/Phi.npy',Phi_np)
+		np.save(data_dir+f'J_data/POD/MPhi.npy',MPhi_np)
+		np.save(data_dir+f'J_data/POD/d_POD.npy',d_POD)
 
 		# Step 2.
 		self.compute_jacobians_in_subspace(derivatives = derivatives, output_basis = phi, output_projector = Mphi,\
