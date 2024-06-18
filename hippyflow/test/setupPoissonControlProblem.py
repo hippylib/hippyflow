b9e9acf01744e428e66a5a393673c85f540971e3
Cypher30 <johnyao@utexas.edu>
2024-06-18 18:15:19 +0800

    remove some docstring

--- hippyflow/test/setupPoissonControlProblem.py
+++ hippyflow/test/setupPoissonControlProblem.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2023, The University of Texas at Austin
-# & Georgia Institute of Technology
-#
-# All Rights reserved.
-# See file COPYRIGHT for details.
-#
-# This file is part of the SOUPy package. For more information see
-# https://github.com/hippylib/soupy/
-#
-# SOUPy is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License (as published by the Free
-# Software Foundation) version 3.0 dated June 2007.
-
-
 import dolfin as dl
 import math
 import numpy as np
